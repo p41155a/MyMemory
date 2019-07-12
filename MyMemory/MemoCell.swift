60c8fdb838a628bacdc9a7f37f09952530440add
comsoft <comsoft@comsoftui-mac-mini-8.local>
2019-07-12 12:38:41 +0900

    commit Thread 1: signal SIGABRT

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCellTableViewCell.swift
+//  MyMemory
+//
+//  Created by comsoft on 2019. 7. 9..
+//  Copyright © 2019년 comsoft. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,19 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by comsoft on 2019. 7. 9..
+//  Copyright © 2019년 comsoft. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class MemoData{
+    var memoIdx : Int? //데이터 식별값
+    var title : String? //메모 제목
+    var contents : String?  //메모 내용
+    var image : UIImage?    //이미지
+    var regdate : Date? //작성일
+}
